7de11805e36eb4ed4472a0d3eba8ce61105b2a80
austinletson <waustinletson@gmail.com>
2024-05-08 12:02:35 -0400

    feat: add support for lean4checker
    Add an input and corresponding step for checking the enviornment
    with lean4checker.
    
    Note, currently this only works for projects which use toolchain/v4.7.0.

---
+++ scripts/run_lean4checker.sh
@@ -0,0 +1,19 @@
+echo "Checking environment with lean4checker"
+
+
+# clone lean4checker
+echo "Cloning and building lean4checker"
+git clone https://github.com/leanprover/lean4checker
+cd lean4checker
+git config --global advice.detachedHead false # turn off git warning about detached head
+git checkout toolchain/v4.7.0
+cp ../lean-toolchain .
+
+# build lean4checker and test lean4checker
+lake build
+./test.sh
+cd ..
+
+# run lean4checker
+echo "Running lean4checker"
+lake env lean4checker/.lake/build/bin/lean4checker
