15dbc9c9c2e57943f47c7bc25e4a50bcb281a4f8
Kim Morrison <kim@tqft.net>
2024-05-09 17:14:08 +1000

    suggestions from ShellCheck

--- scripts/install_elan.sh
+++ scripts/install_elan.sh
@@ -3,4 +3,4 @@
 set -o pipefail
 curl -sSfL https://github.com/leanprover/elan/releases/download/v1.4.2/elan-x86_64-unknown-linux-gnu.tar.gz | tar xz
 ./elan-init -y --default-toolchain none
-echo "$HOME/.elan/bin" >> $GITHUB_PATH
+echo "$HOME/.elan/bin" >> "$GITHUB_PATH"
