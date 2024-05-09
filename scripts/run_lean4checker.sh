e769be8e620ad26a9a8f16487c0d418fc0548db9
Austin Letson <waustinletson@gmail.com>
2024-05-09 18:47:20 +0000

    squash: fix ShellCheck

--- scripts/run_lean4checker.sh
+++ scripts/run_lean4checker.sh
@@ -8,16 +8,16 @@ echo "Cloning and building lean4checker"
 git clone https://github.com/leanprover/lean4checker
 
 do
- (
-    cd lean4checker || exit
-    git config --global advice.detachedHead false # turn off git warning about detached head
-    git checkout toolchain/v4.7.0
-    cp ../lean-toolchain .
+  (
+  cd lean4checker || exit
+  git config --global advice.detachedHead false # turn off git warning about detached head
+  git checkout toolchain/v4.7.0
+  cp ../lean-toolchain .
 
-    # build lean4checker and test lean4checker
-    lake build
-    ./test.sh
- )
+  # build lean4checker and test lean4checker
+  lake build
+  ./test.sh
+  )
 done
 
 # run lean4checker
