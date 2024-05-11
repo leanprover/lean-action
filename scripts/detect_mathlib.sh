c6c4189bb8bd4b87a7ff0d11561f9fdcb8f7c6c0
austinletson <waustinletson@gmail.com>
2024-05-11 09:10:49 -0400

    feat: detect_mathlib.sh supports lakefile.toml

--- scripts/detect_mathlib.sh
+++ scripts/detect_mathlib.sh
@@ -2,8 +2,12 @@
 
 echo "Trying to detect if project is downstream of Mathlib."
 
+# define mathlib dependency patterns for lakefile.lean and lakefile.toml
+dot_lean_pattern="require mathlib"
+dot_toml_pattern="git = \"https://github.com/leanprover-community/mathlib4\""
+
 # Check if the lakefile.lean file contains "require mathlib"
-if grep -q "require mathlib" lakefile.lean; then
+if grep -q "$dot_lean_pattern" lakefile.lean || grep -q "$dot_toml_pattern" lakefile.toml; then
   echo "DETECTED_MATHLIB=true" >> "$GITHUB_OUTPUT"
   echo "Project is downstream of Mathlib. Using Mathlib cache."
 else
