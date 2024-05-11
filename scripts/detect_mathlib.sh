4579e862d8391103325bbb4b6e25650199fa13a9
austinletson <waustinletson@gmail.com>
2024-05-11 09:42:08 -0400

    feat: add more lines to toml mathlib detection

--- scripts/detect_mathlib.sh
+++ scripts/detect_mathlib.sh
@@ -4,10 +4,11 @@ echo "Trying to detect if project is downstream of Mathlib."
 
 # define mathlib dependency patterns for lakefile.lean and lakefile.toml
 dot_lean_pattern="require mathlib"
-dot_toml_pattern="git = \"https://github.com/leanprover-community/mathlib4\""
+dot_toml_pattern="\[\[require\]\]\nname = \"mathlib\"\ngit = \"https://github.com/leanprover-community/mathlib4"
 
-# Check if the lakefile.lean file contains "require mathlib"
-if grep -q "$dot_lean_pattern" lakefile.lean || grep -q "$dot_toml_pattern" lakefile.toml; then
+# Check if lakefile.lean or lakefile.toml contain the mathlib dependency pattern
+if grep -q "$dot_lean_pattern" lakefile.lean || grep -Pzq "$dot_toml_pattern" lakefile.toml; then
+  # Set the DETECTED_MATHLIB variable to true and send it to the GitHub action output to be used in later steps
   echo "DETECTED_MATHLIB=true" >> "$GITHUB_OUTPUT"
   echo "Project is downstream of Mathlib. Using Mathlib cache."
 else
