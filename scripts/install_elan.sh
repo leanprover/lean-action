ae16192c9c96993740d315c1cb3d9bfbfbe7cd6f
Oliver Butterley <51876429+oliver-butterley@users.noreply.github.com>
2024-05-19 19:24:51 +0200

    feat: add logs of lake and lean version (#24)
    Elan installation now:
    - reports the lake version
    - reports the lean version
    
    resolves #12

--- scripts/install_elan.sh
+++ scripts/install_elan.sh
@@ -7,5 +7,7 @@ set -o pipefail
 curl -sSfL https://github.com/leanprover/elan/releases/download/v1.4.2/elan-x86_64-unknown-linux-gnu.tar.gz | tar xz
 ./elan-init -y --default-toolchain none
 echo "$HOME/.elan/bin" >> "$GITHUB_PATH"
+"$HOME"/.elan/bin/lean --version          
+"$HOME"/.elan/bin/lake --version
 
 echo "::endgroup::"
