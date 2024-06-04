dd8bb2b6156de10a9eaf918181143f8f192ec13b
Kitamado <47292598+Seasawher@users.noreply.github.com>
2024-06-04 14:19:53 +0900

    remove `elan-init` file

--- scripts/install_elan.sh
+++ scripts/install_elan.sh
@@ -6,6 +6,8 @@ echo "::group::Elan Installation Output"
 set -o pipefail
 curl -sSfL https://github.com/leanprover/elan/releases/download/v1.4.2/elan-x86_64-unknown-linux-gnu.tar.gz | tar xz
 ./elan-init -y --default-toolchain none
+rm -f elan-init
+
 echo "$HOME/.elan/bin" >> "$GITHUB_PATH"
 "$HOME"/.elan/bin/lean --version          
 "$HOME"/.elan/bin/lake --version
