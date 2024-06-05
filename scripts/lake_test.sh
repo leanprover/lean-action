71e70ad4093fa96030d8e71313b3fc0a64e9e291
Seasawher <tellagoodstory@icloud.com>
2024-06-05 22:44:19 +0900

    feat: don't raise an error if test_runner is not provided

--- scripts/lake_test.sh
+++ scripts/lake_test.sh
@@ -7,12 +7,13 @@ SCRIPT_DIR="$(dirname "$(readlink -f "$0")")"
 # use `lake check-test` to determine if the project has a test runner available
 # TODO: add a more helpful error message once the test frame work matures
 if ! lake check-test; then
-    echo "::error::lake check-test failed: could not find a test runner"
+    echo "lake check-test failed: could not find a test runner"
     cat "$SCRIPT_DIR"/step_summaries/lake_check_error.md >> "$GITHUB_STEP_SUMMARY"
-    exit 1
+else
+    echo "test runner found!"
+    lake test
 fi
 
-lake test
 
 echo "::endgroup::"
 echo
--- scripts/step_summaries/lake_check_error.md
+++ scripts/step_summaries/lake_check_error.md
@@ -1,2 +1,2 @@
 ### `lake check-test` failed
-`lake check-test` could not find a test runner. Add script or lean_exe tagged `@[test_runner]` in the workspace's root package.
+`lake check-test` could not find a test runner. Consider adding script or lean_exe tagged `@[test_runner]` in the workspace's root package.
