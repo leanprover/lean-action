7cca6c63092bd6852e647ab4113d4c1a20a80c41
austinletson <waustinletson@gmail.com>
2024-08-14 08:11:00 -0400

    feat: switch elan installation method to from platform tar -> `elan-init.sh`
    Using `elan-init.sh` will make the `install_elan.sh` script easier to
    maintain and allow lean-action to support additional runners, e.g.
    macos.
    
    Additionally, update all instances of elan installation in the
    functional tests to use `elan-init.sh`

--- scripts/install_elan.sh
+++ scripts/install_elan.sh
@@ -4,12 +4,13 @@
 echo "::group::Elan Installation Output"
 
 set -o pipefail
-curl -sSfL https://github.com/leanprover/elan/releases/download/v3.1.1/elan-x86_64-unknown-linux-gnu.tar.gz | tar xz
-./elan-init -y --default-toolchain none
+# curl -sSfL https://github.com/leanprover/elan/releases/download/v3.1.1/elan-x86_64-unknown-linux-gnu.tar.gz | tar xz
+curl https://raw.githubusercontent.com/leanprover/elan/master/elan-init.sh -sSf |
+  sh -s -- -y --default-toolchain none
 rm -f elan-init
 
-echo "$HOME/.elan/bin" >> "$GITHUB_PATH"
-"$HOME"/.elan/bin/lean --version          
+echo "$HOME/.elan/bin" >>"$GITHUB_PATH"
+"$HOME"/.elan/bin/lean --version
 "$HOME"/.elan/bin/lake --version
 
 echo "::endgroup::"
