c5c17aa1198dc31dcfb6c54c4787d39d7d56c144
austinletson <waustinletson@gmail.com>
2024-08-18 08:08:34 -0400

    refactor: rename DETECTED_MATHLIB -> detected-mathlib

--- scripts/detect_mathlib.sh
+++ scripts/detect_mathlib.sh
@@ -9,15 +9,15 @@ dot_lean_pattern="require mathlib"
 dot_toml_pattern="\[\[require\]\]\nname = \"mathlib\"\ngit = \"https://github.com/leanprover-community/mathlib4"
 # Check if lakefile.lean or lakefile.toml contain the mathlib dependency pattern
 if [ -f lakefile.lean ] && grep -q "$dot_lean_pattern" lakefile.lean; then
-  # Set the DETECTED_MATHLIB variable to true and send it to the GitHub action output to be used in later steps
-  echo "DETECTED_MATHLIB=true" >>"$GITHUB_OUTPUT"
+  # Set the detected-mathlib variable to true and send it to the GitHub action output to be used in later steps
+  echo "detected-mathlib=true" >>"$GITHUB_OUTPUT"
   echo "Detected Mathlib dependency in lakfefile.lean. Using Mathlib cache."
 elif [ -f lakefile.toml ] && grep -Pzq "$dot_toml_pattern" lakefile.toml; then
-  # Set the DETECTED_MATHLIB variable to true and send it to the GitHub action output to be used in later steps
-  echo "DETECTED_MATHLIB=true" >>"$GITHUB_OUTPUT"
+  # Set the detected-mathlib variable to true and send it to the GitHub action output to be used in later steps
+  echo "detected-mathlib=true" >>"$GITHUB_OUTPUT"
   echo "Detected Mathlib dependency in lakfefile.toml. Using Mathlib cache."
 else
-  echo "DETECTED_MATHLIB=false" >>"$GITHUB_OUTPUT"
+  echo "detected-mathlib=false" >>"$GITHUB_OUTPUT"
   echo "Project is not downstream of Mathlib. Skipping Mathlib cache."
 fi
 
