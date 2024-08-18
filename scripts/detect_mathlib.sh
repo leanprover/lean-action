febd43cdfd37f837327b8e4f547acb355968219c
austinletson <waustinletson@gmail.com>
2024-08-18 09:13:41 -0400

    fix: specify entire mathlib repo url

--- scripts/detect_mathlib.sh
+++ scripts/detect_mathlib.sh
@@ -4,8 +4,9 @@
 echo "::group::Detect Mathlib Output"
 echo "Trying to detect if project is downstream of Mathlib."
 
+mathlib_repo_url="https://github.com/leanprover-community/mathlib4"
 # Check if lake-manifest.json contain the mathlib dependency pattern
-if [ -f lake-manifest.json ] && grep -q "mathlib" lake-manifest.json; then
+if [ -f lake-manifest.json ] && grep -q "$mathlib_repo_url" lake-manifest.json; then
   # Set the detected-mathlib variable to true and send it to the GitHub action output to be used in later steps
   echo "detected-mathlib=true" >>"$GITHUB_OUTPUT"
   echo "Detected Mathlib dependency in lake-manifest.json. Using Mathlib cache."
