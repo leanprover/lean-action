32e1daf4c5ca63cef287b9765bed951a855602de
Anne Baanen <Vierkantor@users.noreply.github.com>
2025-06-26 17:28:05 +0200

    Fix comment
    Co-authored-by: Austin Letson <waustinletson@gmail.com>

--- scripts/mk_all_check.sh
+++ scripts/mk_all_check.sh
@@ -4,7 +4,7 @@ set -e
 echo "::group::mk_all Output"
 
 # handle_exit function to handle the exit status of the script
-# and set the test-status output parameter accordingly
+# and set the mk_all-status output parameter accordingly
 handle_exit() {
     exit_status=$?
     if [ $exit_status -ne 0 ]; then
