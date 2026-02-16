78882b955f6953b809086e0c0fa50cc67324b466
SnO2WMaN <me@sno2wman.net>
2026-02-17 01:41:25 +0900

    fix

--- scripts/get_gh_tarball_cache_path.sh
+++ scripts/get_gh_tarball_cache_path.sh
@@ -1,9 +1,14 @@
 #!/usr/bin/env bash
-set -e
+echo "::group::Get GitHub cache tarball path"
 
-PROJECT_NAME=$(jq -r .name lake-manifest.json)
+if [ -f lake-manifest.json ]; then
+  PROJECT_NAME=$(jq -r .name lake-manifest.json)
+  TARBALL_FILE="$PROJECT_NAME-x86_64-unknown-linux-gnu.tar.gz"
 
-TARBALL_FILE="$PROJECT_NAME-x86_64-unknown-linux-gnu.tar.gz"
-echo "Tarball file name: $TARBALL_FILE"
-
-echo "tarball-file=$TARBALL_FILE" >> $GITHUB_OUTPUT
+  echo "Tarball file name: $TARBALL_FILE"
+  echo "tarball-file=$TARBALL_FILE" >> $GITHUB_OUTPUT
+else
+  echo "::error::No lake-manifest.json found. Run lake update to generate manifest"
+  echo "::error::Exiting with status 1"
+  exit 1
+fi
